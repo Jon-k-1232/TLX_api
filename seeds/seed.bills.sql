460b58b85daab0bb611b89a402c09059fd9e313c
Jon Kimmel <jonkimmel16@gmail.com>
2020-03-31 23:53:18 -0700

    DB scripts and seeds added

---
+++ seeds/seed.bills.sql
@@ -0,0 +1,5 @@
+INSERT INTO bills
+    ("tenant","tenantId","manager","managerId","rentFor","dueDate","totalDue","pastDue","basicRent","water","maintenance","amountPaid","paidDate","status","paidWidth")
+VALUES
+    ('Bob Builders, Inc',2,'Monsters, Inc',1,'May 2020','April 15, 2020',2300.00,200.00,1800.00,200.00,300.00,0.00,'April 1, 2020','Outstanding','null'),
+    ('Bob Builders, Inc',2,'Monsters, Inc',1,'April 2020','March 15, 2020',2300.00,000.00,1800.00,200.00,300.00,2100.00,'March 1, 2020','Paid','Check');
---
+++ seeds/seed.messages.sql
@@ -0,0 +1,6 @@
+INSERT INTO contact_info
+    ("date","to","toUserId","from","fromUserId","subject","subjectId","messageContent")
+VALUES
+    ('April 2, 2020','Bob Builders, Inc',2,'Monsters, Inc',1,'Need Plumber',2,'Hi Bob, I called a plumber and they should be out today.'),
+    ('April 1, 2020','Monsters, Inc',1,'Bob Builders, Inc',2,'Need Plumber',2,'Hi Sully, We had a pipe burst and could really use a plumber to come out and repair. The water is currently off.'),
+    ('March 20, 2020','Monsters, Inc',1,'Bob Builders, Inc',2,'Question on Rent',1,'Hi Sully, Rent is going to be a little late sorry.');
