06b0a3c4d213db8e83022e192f408e70962dd90e
Jon Kimmel <jonkimmel16@gmail.com>
2020-07-13 23:32:49 -0700

    alerts post to group

--- src/alerts/alerts-router.js
+++ src/alerts/alerts-router.js
@@ -1,56 +1,72 @@
 const express = require("express");
 const alertsRouter = express.Router();
-//const { requireAuth } = require("../middleware/jwt-auth.js");
+const { requireAuth } = require("../middleware/jwt-auth.js");
 const jsonParser = express.json();
 const alertsService = require("./alerts-service.js");
 
-
 alertsRouter
-    .route("/:group")
-    //.all(requireAuth)
-    .post(jsonParser, async (req, res) => {
-        const {
-            current,
-            expired,
-            urgency,
-            datePosted,
-            dateExpire,
-            to,
-            toUserId,
-            from,
-            fromUserId,
-            groupId,
-            alertMessage,
-        } = req.body;
+  .route("/:group")
+  .all(requireAuth)
 
-        const newAlert = {
-            current,
-            expired,
-            urgency,
-            datePosted,
-            dateExpire,
-            to,
-            toUserId,
-            from,
-            fromUserId,
-            groupId,
-            alertMessage,
-        };
-        const db = req.app.get("db");
+  // Posts new alerts to property group of users
+  .post(jsonParser, async (req, res) => {
+    const db = req.app.get("db");
+    const {
+      current,
+      expired,
+      urgency,
+      datePosted,
+      dateExpire,
+      to,
+      toUserId,
+      from,
+      fromUserId,
+      groupId,
+      alertMessage,
+    } = req.body;
 
-        alertsService
-            //.insertAlert(db, newAlert)
-            .then(() => {
-                alertsService.getCurrentAlert(db, newAlert.groupId).then((currentAlert) => {
-                    res.send({
-                        currentAlert,
-                        message: "Group update successfully.",
-                        status: 200,
-                    });
-                });
+    const newAlert = {
+      current,
+      expired,
+      urgency,
+      datePosted,
+      dateExpire,
+      to,
+      toUserId,
+      from,
+      fromUserId,
+      groupId,
+      alertMessage,
+    };
+
+    // Checks if user is property manager or not. If they are then it proceeds with alert posting. If not then returns unauthorized.
+    alertsService.landlordCheck(db, newAlert.fromUserId).then((manager) => {
+      propertyManager = manager[0].role;
+
+      // Checking that user is a landlord, will deny request if tenant
+      if (propertyManager === "manager") {
+        // Checking database for a Current alert, then deleting in order to make way for new current alert.
+        alertsService.deleteCurrent(db, newAlert.groupId).then(() => {});
+
+        // Inserts new alert into DB and return alert to front end with message and status.
+        alertsService.insertAlert(db, newAlert).then(() => {
+          alertsService
+            .getCurrentAlert(db, newAlert.groupId)
+            .then((currentAlert) => {
+              res.send({
+                currentAlert,
+                message: "Group alert updated successfully.",
+                status: 200,
+              });
             });
-
-
+        });
+      } else {
+        res.send({
+          message: "Unauthorized",
+          status: 401,
+        });
+      }
     });
+  });
 
 module.exports = alertsRouter;
--- src/alerts/alerts-service.js
+++ src/alerts/alerts-service.js
@@ -1,12 +1,26 @@
 const alertsService = {
-    insertAlert(db, newAlert) {
-        return db.insert(newAlert).returning("*").into("alerts");
-    },
+  insertAlert(db, newAlert) {
+    return db.insert(newAlert).returning("*").into("alerts");
+  },
 
-    getCurrentAlert(db, groupId) {
-        return db.select().from("alerts").whereIn("groupId", [groupId]);
-    }
+  deleteCurrent(db, groupId) {
+    return db
+      .select()
+      .from("alerts")
+      .where({ groupId: groupId, current: true })
+      .del();
+  },
 
+  getCurrentAlert(db, groupId) {
+    return db
+      .select()
+      .from("alerts")
+      .where({ groupId: groupId, current: true });
+  },
+
+  landlordCheck(db, userId) {
+    return db.select().from("contact_info").whereIn("userid", [userId]);
+  },
 };
 
 module.exports = alertsService;
